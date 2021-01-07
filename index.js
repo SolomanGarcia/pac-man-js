66323e4ee71ff7d2e6ac81788914914ee20c0de7
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-07 09:58:15 -0700

    work on control function

--- index.js
+++ index.js
@@ -63,3 +63,16 @@ createBoard();
 // starting position for the Pacman
 let pacmanCurrentIndex = 490;
 squares[pacmanCurrentIndex].classList.add("pacman");
+
+function control(e) {
+  if (e.keyCode === 40) {
+    console.log("pressed down");
+  } else if (e.keyCode === 38) {
+    console.log("up");
+  } else if (e.keyCode === 37) {
+    console.log("left");
+  } else if (e.keyCode === 39) {
+    console.log("right");
+  }
+}
+document.addEventListener("keyup", control);
