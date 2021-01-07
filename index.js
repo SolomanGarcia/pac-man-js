a6950c110da8c8c5c2ee3750b50a1dd73cb484eb
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-07 11:06:15 -0700

    enable grid shortcut

--- index.js
+++ index.js
@@ -92,6 +92,9 @@ function control(e) {
         pacmanCurrentIndex % width !== 0
       )
         pacmanCurrentIndex -= 1;
+      if (pacmanCurrentIndex === 364) {
+        pacmanCurrentIndex = 391;
+      }
       break;
     case 39:
       if (
@@ -100,6 +103,9 @@ function control(e) {
         pacmanCurrentIndex % width < width - 1
       )
         pacmanCurrentIndex += 1;
+      if (pacmanCurrentIndex === 391) {
+        pacmanCurrentIndex = 364;
+      }
       break;
   }
   squares[pacmanCurrentIndex].classList.add("pacman");
